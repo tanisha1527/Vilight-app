e96a8369275f60065742476c1fcc483657a0ebfe
Tanisha <vermatanisha620@gmail.com>
2025-03-25 16:06:36 +0530

    Add create post mutation

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,7 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-     Users: defineTable({
+     users: defineTable({
         username: v.string(),
         fullname: v.string(),
         email: v.string(),
