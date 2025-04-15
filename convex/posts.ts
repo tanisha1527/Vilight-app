aa3d624d74b84d1081dabff39f3f9b0d4226478b
Tanisha <vermatanisha620@gmail.com>
2025-04-15 16:07:19 +0530

    Get Feed Posts Query

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,51 +1,88 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { getAuthenticatedUser } from "./users";
 
 
 export const generateUploadUrl = mutation(async (ctx) => {
-      const identity = await ctx.auth.getUserIdentity();
-      if (!identity) throw new Error("Unauthorized");
-      return await ctx.storage.generateUploadUrl();
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized");
+    return await ctx.storage.generateUploadUrl();
 });
 
 export const createPost = mutation({
-    args:{
-         caption: v.optional(v.string()),
-         storageId: v.id("_storage"),
+    args: {
+        caption: v.optional(v.string()),
+        storageId: v.id("_storage"),
     },
 
-    handler: async (ctx,args) => {
-      const identity = await ctx.auth.getUserIdentity();
-      if (!identity) throw new Error("Unauthorized");
+    handler: async (ctx, args) => {
+        const currentUser = await getAuthenticatedUser(ctx);
 
-      const currentUser = await ctx.db
-      .query("users")
-      .withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
-      .first();
+        const imageUrl = await ctx.storage.getUrl(args.storageId);
+        if (!imageUrl) throw new Error("Image not found");
 
-      if(!currentUser) throw new Error("User not found")
+        // createPost
 
-         const imageUrl = await ctx.storage.getUrl(args.storageId);
-         if(!imageUrl) throw new Error("Image not found");
-         
-         // createPost
+        const postId = await ctx.db.insert("posts", {
+            userId: currentUser._id,
+            imageUrl,
+            storageId: args.storageId,
+            caption: args.caption,
+            likes: 0,
+            comments: 0
+        })
 
-         const postId = await ctx.db.insert("posts", {
-             userId: currentUser._id,
-             imageUrl,
-             storageId: args.storageId,
-             caption: args.caption,
-             likes: 0,
-             comments: 0        
-         })
+        // Increment user's post count by 1
 
-         // Increment user's post count by 1
-
-         await ctx.db.patch(currentUser._id, {
-             posts: currentUser.posts + 1
-         })
+        await ctx.db.patch(currentUser._id, {
+            posts: currentUser.posts + 1
+        });
 
         return postId;
 
     },
-}) 
+})
+
+export const getFeedPosts = query({
+    handler: async (ctx) => {
+        const currentUser = await getAuthenticatedUser(ctx);
+
+        // get all posts
+        const posts = await ctx.db.query("posts").order("desc").collect()
+
+        if (posts.length === 0) return []
+
+        // enhance posts with userdata and interaction stattus
+        const postsWithInfo = await Promise.all(
+            posts.map(async (post) => {
+                const postAuthor = await ctx.db.get(post.userId)
+
+                const like = await ctx.db
+                    .query("likes")
+                    .withIndex("by_user_and_post",
+                        (q) => q.eq("userId", currentUser._id).eq("postId", post._id))
+                    .first();
+
+                const bookmark = await ctx.db
+                    .query("bookmarks")
+                    .withIndex("by_user_and_post",
+                        (q) => q.eq("userId", currentUser._id).eq("postId", post._id))
+                    .first();
+
+                return {
+                    ...post,
+                    author: {
+                        _id: postAuthor?._id,
+                        username: postAuthor?.username,
+                        image: postAuthor?.image
+                    },
+                    isLiked: !!like,
+                    isBookmarked: !!bookmark
+                }
+
+            })
+        )
+
+        return postsWithInfo;
+    },
+})
--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation, MutationCtx, QueryCtx } from "./_generated/server";
 import { v } from "convex/values";
 
 // Create a new task with the given text
@@ -33,3 +33,19 @@ export const createUser = mutation({
           })
      }
 });
+
+export async function getAuthenticatedUser(ctx:QueryCtx | MutationCtx) {
+
+     const identity = await ctx.auth.getUserIdentity();
+      if (!identity) throw new Error("Unauthorized");
+
+      const currentUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
+      .first();
+
+      if(!currentUser) throw new Error("User not found");
+
+      return currentUser;
+
+}
