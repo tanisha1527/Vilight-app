e737569647d40a74c8dcdae268fd2f52e009e1ec
Tanisha <vermatanisha620@gmail.com>
2025-04-16 00:52:25 +0530

    Fetching Posts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,64 @@
-import { Image, Pressable, Text, TouchableOpacity, View, } from "react-native";
-import { styles } from "../../styles/auth.styles";
+import { Image, Pressable, ScrollView, Text, TouchableOpacity, View, } from "react-native";
+import { styles } from "../../styles/feed.styles";
 import { Link } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+import { STORIES } from "@/constants/mock-data";
+import Story from "@/components/Story";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Loader } from "@/components/Loader";
+import Post from "@/components/Post";
 
 export default function Index() {
-  const {signOut} = useAuth()
+  const {signOut} = useAuth();
+
+  const posts = useQuery(api.posts.getFeedPosts)
+
+  if(posts === undefined) return <Loader />
+  if(posts.length === 0) return <NoPostsFound />
 
   return (
-    <View style={styles.container} >
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text style={{ color:"white"}}>
-          sign-out
-        </Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+        {/* HEADER */ }
+         <View style={styles.header}>
+            <Text style={styles.headerTitle}>Vilight</Text>
+             <TouchableOpacity onPress={() => signOut()}>
+              <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+             </TouchableOpacity>
+         </View>
+
+         <ScrollView 
+         showsVerticalScrollIndicator={false}
+         contentContainerStyle={{paddingBottom: 60}}
+         >
+               
+               {/* STORIES */}
+               <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.storiesContainer}>
+                   {STORIES.map((story) => (
+                      <Story key={story.id} story={story} />
+                   ))}
+               </ScrollView>
+                 
+               {posts.map((post) => (
+                   <Post key={post._id} post={post} />
+               ))}
+
+         </ScrollView>
     </View>
   );
 }
+
+
+const NoPostsFound = () => (
+    <View style={{
+       flex:1,
+       backgroundColor: COLORS.background,
+       justifyContent: "center",
+       alignItems: "center",
+    }}>
+      <Text style={{ fontSize:20, color: COLORS.primary }}>No posts yet</Text>
+    </View>
+);
 
---
+++ constants/mock-data.ts
@@ -0,0 +1,50 @@
+export const STORIES = [
+     {
+        id: "1",
+        username: "You",
+        avatar: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=400&fit=crop",
+        hasStory: false,
+     },
+     {
+        id: "2",
+        username: "Rehana",
+        avatar: "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?w=400&fit=crop",
+        hasStory: true,
+     },
+     {
+        id: "3",
+        username: "Saleena",
+        avatar: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?w=400&fit=crop",
+        hasStory: true,
+     },
+     {
+        id: "4",
+        username: "Nicolas Hans",
+        avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=400&fit=crop",
+        hasStory: true,
+     },
+     {
+        id: "5",
+        username: "Diego Harnendaz",
+        avatar: "https://images.unsplash.com/photo-1552058544-f2b08422138a?w=400&fit=crop",
+        hasStory: true,
+     },
+     {
+        id: "6",
+        username: "Aiony Haust",
+        avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&fit=crop",
+        hasStory: true,
+     },
+     {
+        id: "7",
+        username: "Oleg Lvanov",
+        avatar: "https://images.unsplash.com/photo-1517841905240-472988babdf9?w=400&fit=crop",
+        hasStory: true,
+     },
+     {
+        id: "8",
+        username: "Christiana",
+        avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=400&fit=crop",
+        hasStory: true,
+     },
+];
