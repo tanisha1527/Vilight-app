f21110f8f65fc00ebb4ac024e550ac318c4ef3e7
Tanisha <vermatanisha620@gmail.com>
2025-04-27 23:30:55 +0530

    Clean up code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,14 @@
-import { FlatList, Image, Pressable, ScrollView, Text, TouchableOpacity, View, } from "react-native";
-import { styles } from "../../styles/feed.styles";
-import { Link } from "expo-router";
-import { useAuth } from "@clerk/clerk-expo";
-import { Ionicons } from "@expo/vector-icons";
-import { COLORS } from "@/constants/theme";
-import { STORIES } from "@/constants/mock-data";
-import Story from "@/components/Story";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import { Loader } from "@/components/Loader";
 import Post from "@/components/Post";
+import StoriesSection from "@/components/Stories";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/feed.styles";
+import NoPostsFound from "@/components/NoPost";
 
 export default function Index() {
   const {signOut} = useAuth();
@@ -40,25 +39,3 @@ export default function Index() {
     </View>
   );
 }
-
-const StoriesSection = () => {
-    return (
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.storiesContainer}>
-      {STORIES.map((story) => (
-        <Story key={story.id} story={story} />
-      ))}
-      </ScrollView>
-    )
-}
-
-const NoPostsFound = () => (
-    <View style={{
-       flex:1,
-       backgroundColor: COLORS.background,
-       justifyContent: "center",
-       alignItems: "center",
-    }}>
-      <Text style={{ fontSize:20, color: COLORS.primary }}>No posts yet</Text>
-    </View>
-);
-
